838e5ca3e29ca24131d43afd9b02457deaff372d
Anisha Shrestha <shrestan314@gmail.com>
2019-01-29 20:13:20 -0600

    Added a comment with my name

--- app/src/main/java/com/pleasedo/planner/Welcome_Activity.java
+++ app/src/main/java/com/pleasedo/planner/Welcome_Activity.java
@@ -6,7 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-//Team Members: Anisha Shrestha
+//Team Members: Anisha Shresthasd
 public class Welcome_Activity extends AppCompatActivity {
 
 
