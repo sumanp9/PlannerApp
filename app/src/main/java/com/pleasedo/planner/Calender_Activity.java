8c158f29fced1436c7bfc92aa2da9867a4f8ee2d
sumanp9 <11.sumanpradhan@gmail.com>
2019-01-29 20:31:45 -0600

    Added a interaction in which we can get the selected calenderDate.                -Suman

--- app/src/main/java/com/pleasedo/planner/Calender_Activity.java
+++ app/src/main/java/com/pleasedo/planner/Calender_Activity.java
@@ -1,13 +1,39 @@
 package com.pleasedo.planner;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Date;
 
 public class Calender_Activity extends AppCompatActivity {
+
+    private static CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
+
+        calendarView = (CalendarView)findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String selectedDate = month+1+"/"+dayOfMonth+"/"+year ;
+                //Hint: convert String selectedDate to type Date and compaare
+                //it with the today's date. If the selectedDate is lesser or in the past then
+                //allow user to select the date again. if not proceed as normal.
+                Intent intent = new Intent(Calender_Activity.this, MainActivity.class);
+                intent.putExtra("date",selectedDate);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
