56eedb494e333480c375f940c929aa4e2a5e48c6
sumanp9 <11.sumanpradhan@gmail.com>
2019-02-04 19:28:57 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/pleasedo/planner/Welcome_Activity.java

--- app/src/main/java/com/pleasedo/planner/Welcome_Activity.java
+++ app/src/main/java/com/pleasedo/planner/Welcome_Activity.java
@@ -1,31 +1,48 @@
 package com.pleasedo.planner;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
 //Team Members: Anisha Shrestha
 public class Welcome_Activity extends AppCompatActivity {
 
+    private static FloatingActionMenu menu;
+    private  static FloatingActionButton floatingEvent, floatingNote;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        floatingEvent = (FloatingActionButton)findViewById(R.id.floatingEvent);
+        floatingNote = (FloatingActionButton)findViewById(R.id.floatingNote);
+
+        floatingNote.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(Welcome_Activity.this, MainActivity.class);
+                intent.putExtra("note","Note");
             }
         });
+
+        floatingEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Welcome_Activity.this, MainActivity.class);
+                intent.putExtra("note","Event");
+            }
+        });
+
+
+
     }
+
+
 
 }
