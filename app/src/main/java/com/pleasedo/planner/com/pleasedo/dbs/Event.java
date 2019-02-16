03ac900307a1be79836e1cebd46d5587f0e55950
sumanp9 <11.sumanpradhan@gmail.com>
2019-02-15 23:47:30 -0600

    added function classes note and event

---
+++ app/src/main/java/com/pleasedo/planner/com/pleasedo/dbs/Event.java
@@ -0,0 +1,60 @@
+package com.pleasedo.planner.com.pleasedo.dbs;
+
+public class Event {
+
+    private String eventID;
+    private String eventTitle;
+    private String eventDetails;
+    private String eventStartDate;
+    private String eventEndDate;
+
+
+    public Event(){}
+
+    public Event(String evenTitle, String eventDetails, String eventStartDate, String eventEndDate){
+        this.eventTitle = evenTitle;
+        this.eventDetails =eventDetails;
+        this.eventStartDate = eventStartDate;
+        this.eventEndDate = eventEndDate;
+    }
+
+    public String getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(String eventID) {
+        this.eventID = eventID;
+    }
+
+    public String getEventTitle() {
+        return eventTitle;
+    }
+
+    public void setEventTitle(String eventTitle) {
+        this.eventTitle = eventTitle;
+    }
+
+    public String getEventDetails() {
+        return eventDetails;
+    }
+
+    public void setEventDetails(String eventDetails) {
+        this.eventDetails = eventDetails;
+    }
+
+    public String getEventStartDate() {
+        return eventStartDate;
+    }
+
+    public void setEventStartDate(String eventStartDate) {
+        this.eventStartDate = eventStartDate;
+    }
+
+    public String getEventEndDate() {
+        return eventEndDate;
+    }
+
+    public void setEventEndDate(String eventEndDate) {
+        this.eventEndDate = eventEndDate;
+    }
+}
