75c38bbcb18dc0e567ad031211ca3662aace308b
sumanp9 <11.sumanpradhan@gmail.com>
2019-02-18 19:10:07 -0600

    added mote db function

--- app/src/main/java/com/pleasedo/planner/loginDB.java
+++ app/src/main/java/com/pleasedo/planner/loginDB.java
@@ -5,8 +5,11 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.ArrayAdapter;
 
 import com.pleasedo.dbClass.Login;
+
+import java.util.ArrayList;
 
 public class loginDB extends SQLiteOpenHelper {
 
@@ -54,6 +57,30 @@ public class loginDB extends SQLiteOpenHelper {
         db.close();
     }
 
+    public ArrayList<String> userList(){
+        ArrayList<String> userArray = new ArrayList<String>();
+        SQLiteDatabase db =  getWritableDatabase();
+        String query = "SELECT * FROM "+ TABLE_LOGIN + " WHERE 1";
+        Cursor c =  db.rawQuery(query,null);
+
+        //Move to the first row in your results
+        c.moveToFirst();
+
+        while(!c.isAfterLast()){
+            if (c.getString(c.getColumnIndex("Username"))!=null){
+
+                userArray.add(c.getString(c.getColumnIndex("Username")));
+
+            }
+            c.moveToNext();
+        }
+
+
+        db.close();
+
+        return userArray;
+    }
+
     public String databaseToString(){
         String dbString= "";
         SQLiteDatabase db =  getWritableDatabase();
@@ -82,6 +109,7 @@ public class loginDB extends SQLiteOpenHelper {
         return dbString;
 
     }
+
 
 }
 
