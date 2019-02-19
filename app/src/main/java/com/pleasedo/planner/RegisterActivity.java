7926a5e632549c6621a64a8ee0bf8fc665f22af3
Anisha Shrestha <shrestan314@gmail.com>
2019-02-18 19:33:12 -0600

    another committed

--- app/src/main/java/com/pleasedo/planner/RegisterActivity.java
+++ app/src/main/java/com/pleasedo/planner/RegisterActivity.java
@@ -2,13 +2,41 @@ package com.pleasedo.planner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private static EditText FirstName, LastName,
+            UserName, Email, Password, RetypePassword;
+    private static Button Register;
+    loginDB newHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        FirstName = (EditText) findViewById(R.id.FirstNameTV);
+        LastName = (EditText) findViewById(R.id.LastNameTV);
+        UserName = (EditText) findViewById(R.id.usernameTV);
+        Email = (EditText) findViewById(R.id.email);
+        Password = (EditText)findViewById(R.id.password);
+        RetypePassword = (EditText)findViewById(R.id.password2);
+        Register = (Button)findViewById(R.id.registerBtn);
+
+        checkUsername();
+    }
+
+    private void checkUsername() {
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String>userArray = new ArrayList<String>();
+
+            }
+        });
     }
 }
